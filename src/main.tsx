22484692d7f0bfbcc2b0c5a8bb25f68889f938a0
Oleg Ganus <oleg110770@gmail.com>
2025-08-08 17:27:39 +0300

    edit error

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,12 @@ import App from "./components/App/App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-const querryClient = new QueryClient()
+const queryClient = new QueryClient()
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={querryClient}>
+    <QueryClientProvider client={queryClient}>
       <App />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
